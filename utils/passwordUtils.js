655b404a51bac9cd43eeb550135e13b634521ca4
Raymond Nguyen <125716839+Fenaum@users.noreply.github.com>
2023-11-04 15:18:42 -0700

    refactored bycrupt

--- utils/passwordUtils.js
+++ utils/passwordUtils.js
@@ -1,25 +1,23 @@
-const bcrypt = require('bcrypt')
+const bcrypt = require("bcrypt");
 
-const plaintextPassword = 'user_password';
+const plaintextPassword = "user_password";
 
 // Hash the Password
-
 bcrypt.hash(plaintextPassword, 10, (err, hash) => {
-    if(err) {
-        console.error(err, {message: "Invalid Password"})
-    } else {
-        console.log(hash)
-    }
-});
+  if (err) {
+    console.error(err, { message: "Invalid Password" });
+  } else {
+    console.log(hash);
 
-// Verify the password
-
-bcrypt.compare(plaintextPassword, hash, (err, result) => {
-    if(err) {
-        console.error(err, {message: "Invalid Password"})
-    } else if (result) {
-        console.log('Success')
-    } else {
-        console.log('Incorrect Password')
-    }
+    // Verify the password
+    bcrypt.compare(plaintextPassword, hash, (err, result) => {
+      if (err) {
+        console.error(err, { message: "Invalid Password" });
+      } else if (result) {
+        console.log("Success");
+      } else {
+        console.log("Incorrect Password");
+      }
+    });
+  }
 });
