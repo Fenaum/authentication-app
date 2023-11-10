7ac9a66af3771d217ba04a6c7146054df4b7a425
Raymond Nguyen <125716839+Fenaum@users.noreply.github.com>
2023-11-10 13:28:06 -0800

    passport set up

--- app.js
+++ app.js
@@ -1,13 +1,21 @@
 const express = require("express");
 const passport = require("./config/passport"); // Your passport configuration
 const session = require("express-session");
-const routes = require('./routes/auth.js')
-const mongoose = require('mongoose')
+const MongoStore = require("connect-mongo");
+const routes = require("./routes/auth.js");
+const mongoose = require("mongoose");
+
+// Connect to MongoDB
+mongoose.connect("mongodb://localhost/authenticator", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 const app = express();
 
 // Express body parser
 app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 // Express session
 app.use(
@@ -15,6 +23,7 @@ app.use(
     secret: "secret", // Replace 'secret' with your own secret key
     resave: true,
     saveUninitialized: true,
+    store: MongoStore.create({ mongoUrl: "mongodb://localhost/authenticator" }), // Use MongoStore and pass the mongoose connection
   })
 );
 
@@ -23,7 +32,7 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 // Routes
-app.use('/', routes)
+app.use("/", routes);
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, console.log(`Server started on port ${PORT}`));
--- controllers/authController.js
+++ controllers/authController.js
@@ -1,20 +1,30 @@
 const bcrypt = require("../utils/passwordUtils");
-const User = require('../models/User.js'); // Assuming you have a User model
+const User = require("../models/User.js"); // Assuming you have a User model
 
 const authController = {
   async userRegistration(req, res) {
     const { username, password } = req.body;
 
+    // Check if password is provided
+    if (!password) {
+      return res.status(400).json({ error: "Password is required" });
+    }
+
     try {
       // Hash the password
       const hash = await bcrypt.hash(password, 10);
 
+      console.log("Hashed Password:", hash); // Add this line to log the hashed password
+
       // Create new user
       const user = await User.create({ username, password: hash });
+
+      console.log("Created User:", user); // Add this line to log the created user
 
       // Automatically log the user in after registration
       req.login(user, (err) => {
         if (err) {
+          console.error("Login Error:", err); // Add this line to log login errors
           return res
             .status(500)
             .json({ error: "Error logging in after registration" });
@@ -25,14 +35,16 @@ const authController = {
           .json({ message: "User registration successful" });
       });
     } catch (err) {
+      console.error("Registration Error:", err); // Add this line to log registration errors
       return res.status(500).json({ error: "Error creating new user" });
     }
   },
-    userLogin(req, res) {
+
+  userLogin(req, res) {
     return res.status(200).json({ message: "User login successful" });
   },
 
-    userLogout(req, res) {
+  userLogout(req, res) {
     req.logout();
     return res.status(200).json({ message: "User logout successful" });
   },
